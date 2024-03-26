067068c00f8b9d8727352d050bd76496ab5a0cb2
999.kings <hoooolysht@gmail.com>
2024-03-27 01:03:27 +0300

    completed: router and dashboard

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,10 +2,10 @@ import { useState }  from "react";
 import { Link } from "react-router-dom";
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
+import SportsKabaddiSharpIcon from '@mui/icons-material/SportsKabaddiSharp';
 
-type Props = {}
 
-const Navbar =  (props:Props) => {
+const Navbar =  () => {
     const { palette } = useTheme()
     const [ selected, setSelected] = useState("dashboard")
     return <FlexBetween 
@@ -15,7 +15,38 @@ const Navbar =  (props:Props) => {
             >
                 {/* LEFT SIDE */}
                 <FlexBetween gap="0.75rem">
+                    <SportsKabaddiSharpIcon sx={{ fontSize: "28px"}}/>
+                    <Typography variant="h4" fontSize="16px">
+                        Fintech
+                    </Typography>
+                </FlexBetween>
 
+                {/* RIGHT SIDE */}
+                <FlexBetween gap="2rem">
+                    <Box sx={{ "&:hover": { color: palette.primary[100]}}}>
+                        <Link 
+                            to="/"
+                            onClick={() => setSelected("dashboard")}
+                            style={{
+                                color: selected === "dashboard" ? "inherit" : palette.grey[700],
+                                textDecoration: "inherit"
+                            }}
+                            >
+                                Dashboard
+                        </Link>
+                    </Box>
+                    <Box sx={{ "&:hover": { color: palette.primary[100]}}}>
+                        <Link 
+                            to="/predictions"
+                            onClick={() => setSelected("predictions")}
+                            style={{
+                                color: selected === "predictions" ? "inherit" : palette.grey[700],
+                                textDecoration: "inherit"
+                            }}
+                            >
+                                Predictions
+                        </Link>
+                    </Box>
                 </FlexBetween>
             </FlexBetween>
 }
