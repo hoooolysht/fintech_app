c1887d86d88606bec714da7b8c8f3b929c98d4b0
999.kings <hoooolysht@gmail.com>
2024-04-27 00:24:39 +0300

    recharts and key perfomance indicators ui

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,16 +1,64 @@
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKPIsQuery } from '@/state/api'
-import React from 'react'
+import { useTheme } from '@mui/material'
+import React, { useMemo } from 'react'
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 type Props = {}
 
 const Row1 = (props: Props) => {
-  const { data } = useGetKPIsQuery();
+  const { palette } = useTheme();
+  const { data, error, isLoading } = useGetKPIsQuery();
+  // if (isLoading) {
+  //   return <div>Loading...</div>
+  // };
+  // if (error) {
+  //   return <div>Error</div>
+  // };
   console.log("data:", data);
+  const revenueExpenses = useMemo(() => {
+    return (
+      data && 
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses,
+        }
+      })
+    );
+  }, [data]);
 
   return (
     <>
-    <DashboardBox gridArea="a"></DashboardBox>
+    <DashboardBox gridArea="a">
+    <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={revenueExpenses}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area 
+            type="monotone" 
+            dataKey="revenue" 
+            stroke={ palette.primary.main }
+            fillOpacity={1}
+            fill="url(#colorRevenue)" 
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+
+    </DashboardBox>
     <DashboardBox gridArea="b"></DashboardBox>
     <DashboardBox gridArea="c"></DashboardBox>
     </>
--- src/state/api.ts
+++ src/state/api.ts
@@ -1,12 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetKpisResponse } from "./types";
+
+// Define a service using a base URL and expected endpoints
 
 export  const api = createApi({
         baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
         reducerPath: "main",
         tagTypes: ["KPIs"],
-        endpoints: (builder) => ({
+        endpoints: (build) => ({
             // this is a template for receiving APIs
-            getKPIs: builder.query<void, void>({
+            getKPIs: build.query <Array<GetKpisResponse>, void>({
                 query: () => "kpi/kpis/",
                 providesTags: ["KPIs"],
             }) 
