14d711a87ad2b9fc40bf2a0e1e47dc59806a356d
999.kings <hoooolysht@gmail.com>
2024-05-28 23:58:01 +0300

    completed the dachboard. go to the predictions page

--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -1,19 +1,43 @@
 import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
 import {
   useGetKPIsQuery,
   useGetProductsQuery,
   useGetTransactionsQuery,
 } from "@/state/api";
-import { Box, useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid, GridCellParams } from "@mui/x-data-grid";
-import React from "react";
+import React, { useMemo } from "react";
+import { Cell, Pie, PieChart } from "recharts";
 
 const Row3 = () => {
   const { palette } = useTheme();
+  const pieColor = [palette.primary[800], palette.primary[300]];
   const { data: kpiData } = useGetKPIsQuery();
   const { data: productData } = useGetProductsQuery();
   const { data: transactionsData } = useGetTransactionsQuery();
+
+  const pieChartData = useMemo(() => {
+    if (kpiData) {
+      const totalExpenses = kpiData[0].totalExpenses;
+      return Object.entries(kpiData[0].expensesByCategory).map(
+        ([key, value]) => {
+          return [
+            {
+              name: key,
+              value: value,
+            },
+            {
+              name: `${key} of Total`,
+              value: totalExpenses - value,
+            },
+          ];
+        }
+      );
+    }
+  }, [kpiData]);
+
   const productColumns = [
     {
       field: "_id",
@@ -31,6 +55,31 @@ const Row3 = () => {
       headerName: "Price",
       flex: 0.5,
       renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+  ];
+  const transactionsColumns = [
+    {
+      field: "_id",
+      headerName: "id",
+      flex: 1,
+    },
+    {
+      field: "buyer",
+      headerName: "Buyer",
+      flex: 0.67,
+    },
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 0.35,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: "productIds",
+      headerName: "Count",
+      flex: 0.35,
+      renderCell: (params: GridCellParams) =>
+        (params.value as Array<string>).length,
     },
   ];
 
@@ -70,9 +119,87 @@ const Row3 = () => {
           />
         </Box>
       </DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="h">
+        <BoxHeader
+          title="List of Orders"
+          sideText={`${transactionsData?.length} latest transactions`}
+        />
+        <Box
+          mt="1rem"
+          p="0 0.5rem"
+          height="80%"
+          sx={{
+            "& .MuiDataGrid-root": {
+              color: palette.grey[300],
+              border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnSeparator": {
+              visibility: "hidden",
+            },
+          }}
+        >
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={transactionsData || []}
+            columns={transactionsColumns}
+          />
+        </Box>
+      </DashboardBox>
+      <DashboardBox gridArea="i">
+        <BoxHeader title="Expense Breakdown by Category" sideText="+4%" />
+        <FlexBetween mt="0.5rem" gap="0.5rem" p="0 1rem" textAlign="center">
+          {pieChartData?.map((data, i) => (
+            <Box key={`${data[0].name}-${i}`}>
+              <PieChart width={110} height={100}>
+                <Pie
+                  stroke="none"
+                  data={data}
+                  innerRadius={18}
+                  outerRadius={35}
+                  paddingAngle={2}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={pieColor[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
+              <Typography variant="h5">{data[0].name} </Typography>
+            </Box>
+          ))}
+        </FlexBetween>
+      </DashboardBox>
+      <DashboardBox gridArea="j">
+        <BoxHeader
+          title="Overall Summary and Explanation Data"
+          sideText="+4%"
+        />
+        <Box
+          height="15px"
+          margin="1.25rem 1rem 0.4rem 1rem"
+          bgcolor={palette.primary[800]}
+          borderRadius="1rem"
+        >
+          <Box
+            height="15px"
+            bgcolor={palette.primary[600]}
+            borderRadius="1rem"
+            width="40%"
+          ></Box>
+        </Box>
+        <Typography margin="0 1rem" variant="h6">
+          Не специально придуманный некоторый текст для этого меню. Наличие
+          текста необязательно, но тут он есть. Написано 28.05.2024
+        </Typography>
+      </DashboardBox>
     </>
   );
 };
--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -3,8 +3,7 @@ import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
 
-
-const  gridTemplateLargeScreens = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -15,7 +14,7 @@ const  gridTemplateLargeScreens = `
     "g h i"
     "g h j"
     "g h j"
-`; 
+`;
 const gridTemplateSmallScreens = `
     "a"
     "a"
@@ -50,29 +49,33 @@ const gridTemplateSmallScreens = `
 
 `;
 
-
-
 const Dashboard = () => {
-    const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
-    return (
-      <Box width="100%" height="100%" display="grid" gap="1.5rem"
-        sx={
-            isAboveMediumScreens ? {
-                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-                gridTemplateAreas: gridTemplateLargeScreens,
-            } : {
-                gridAutoColumns: "1fr",
-                gridAutoRows: "80px",
-                gridTemplateAreas: gridTemplateSmallScreens,
+  const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
             }
-        }
-      >
-        <Row1 />
-        <Row2 />
-        <Row3 />
-      </Box>
-    );
-}
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Row1 />
+      <Row2 />
+      <Row3 />
+    </Box>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
